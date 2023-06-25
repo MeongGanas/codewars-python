c0f3be79525808c5d5e39890bbbfe77154c534bf
MeongGanas <farreluken@gmail.com>
2023-06-25 13:10:24 +0800

    ok

---
+++ string_incrementer.py
@@ -0,0 +1,7 @@
+def increment_string(strng):
+    num_list = list(map(str, range(0, 10)))
+
+
+print(increment_string("foo"))
+print(increment_string("foobar23"))
+print(increment_string("foobar0043"))

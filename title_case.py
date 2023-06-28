a007474514c7c0ce10c4e5e5dc4352df45dc9928
MeongGanas <farreluken@gmail.com>
2023-06-28 19:40:27 +0800

    title case

---
+++ title_case.py
@@ -0,0 +1,8 @@
+def title_case(title, minor_words=''):
+    title = title.capitalize().split()
+    minor_words = minor_words.lower().split()
+    return ' '.join([word if word in minor_words else word.capitalize() for word in title])
+
+
+print(title_case('a clash of KINGS', 'a an the of'))
+print(title_case('THE WIND IN THE WILLOWS', 'The In'))

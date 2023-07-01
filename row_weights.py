58b56eced7585f234f49c7d98f31ad6eb889b42f
MeongGanas <farreluken@gmail.com>
2023-07-01 21:40:05 +0800

    ok

--- row_weights.py
+++ row_weights.py
@@ -1,12 +1,6 @@
 def row_weights(array):
     # your code here
-    res = [0, 0]
-    for i, v in enumerate(array):
-        if i % 2 == 0:
-            res[0] += v
-        else:
-            res[1] += v
-    return tuple(res)
+    return sum(array[::2]), sum(array[1::2])
 
 
 print(row_weights([50, 60, 70, 80]))

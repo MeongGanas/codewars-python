8c433abd68abecfd6f4e0833222a1081801b59c2
MeongGanas <farreluken@gmail.com>
2023-07-09 11:41:56 +0800

    ok

--- rule_divisibility.py
+++ rule_divisibility.py
@@ -16,4 +16,15 @@ def thirt(n):
         n = current_sum
 
 
+array = [1, 10, 9, 12, 3, 4]
+
+
+def thirt(n):
+    total = sum([int(c) * array[i % 6]
+                for i, c in enumerate(reversed(str(n)))])
+    if n == total:
+        return total
+    return thirt(total)
+
+
 print(thirt(1234567))
